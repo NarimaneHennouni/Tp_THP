e6adbbd1c27051de2c4dc2d5ae759d193e04703b
narimane00 <47577315+narimane00@users.noreply.github.com>
2020-03-13 23:18:51 +0100

    Add files via upload

---
+++ TP_THP/Main.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+
+public class Main {
+     public static void main(String[] args) {
+         CreerAutomateSimple();
+         System.out.println("Veuillez entrer votre Automate");
+         System.out.println("");
+         System.out.println("Choisir ce que vous voulez faire sur votre automate:");
+         System.out.println(" 1.Réduire l'automate");                  
+         System.out.println(" 2.Faire le complement"); 
+         System.out.println(" 3.Faire le miroir");
+         System.out.println(" 4.Vérifier la reconnaissance de mots");
+         System.out.println(" 5.Déterminiser l'automate");                 
+                                              
+        Scanner sc= new Scanner(System.in);
+        int i=sc.nextInt();
+        switch(i)
+        {
+            case 1:
+            break;
+
+            case 2:
+            break;
+
+            case 3:
+            break;
+
+            case 4:
+            break;
+
+            case 5:
+            break;
+
+        }
+        
+    }
+
+
+
+
+
+}
